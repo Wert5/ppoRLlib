26a6f6e1eedf0264933a7279d186cbe6f16d3012
Winston Grenier <winston@strategyrobot.ai>
2023-10-04 14:13:44 -0400

    Switch to hyperparameters from test insteadof outdated experiment

--- evalScript.py
+++ evalScript.py
@@ -4,25 +4,23 @@ from ray.rllib.algorithms import Algorithm
 
 config = (  # 1. Configure the algorithm,
     PPOConfig()
-    .environment("BreakoutNoFrameskip-v4",
-        clip_rewards=True, clip_actions=True)
+    .rl_module(_enable_rl_module_api=True)
+    .environment("BreakoutNoFrameskip-v4", clip_rewards=True,
+        env_config={"frameskip" : 1, "full_action_space" : False,
+        "repeat_action_probability" : 0.0})
     .resources(num_gpus=1, num_gpus_per_learner_worker=1)
     .rollouts(num_rollout_workers=5, num_envs_per_worker=5,
-        observation_filter="NoFilter",
-        batch_mode="truncate_episodes")
+        rollout_fragment_length="auto")
     .framework("torch")
-    .training(model={"vf_share_layers": True, "conv_filters": None,
-            "conv_activation": "relu", "fcnet_activation": "tanh",
-            "fcnet_hiddens": [256, 256], "free_log_std": False,
-            "no_final_linear": False, "use_lstm": False, "framestack": True,
-            "dim": 84, "grayscale": False, "zero_mean": True,
-            "custom_model": None},
-        entropy_coeff=0.01, train_batch_size=5000, num_sgd_iter=10,
-        lr_schedule=[[0, 5e-5]],
-        vf_clip_param=10.0, kl_coeff=0.5, lambda_=0.95,
-        clip_param=0.1, sgd_minibatch_size=500)
+    .training(_enable_learner_api=True, model={"vf_share_layers": True,
+            "conv_filters" : [[16, 4, 2], [32, 4, 2], [64, 4, 2], [128, 4, 2]],
+            "conv_activation": "relu", "post_fcnet_hiddens": [256]},
+        entropy_coeff=0.01, train_batch_size=4000, num_sgd_iter=10,
+        lr_schedule=[[0, 1e-4]], vf_clip_param=10.0, kl_coeff=0.5,
+        lambda_=0.95, clip_param=0.1, sgd_minibatch_size=1000,
+        grad_clip=100.0, grad_clip_by="global_norm")
     .evaluation(evaluation_num_workers=5, evaluation_duration=100,
-        evaluation_interval=10)
+        evaluation_interval=5)
 )
 
 algo = config.build()  # 2. build the algorithm,
--- ppoExample1.py
+++ ppoExample1.py
@@ -2,27 +2,23 @@ from pprint import pprint
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.algorithms import Algorithm
 
-max_timesteps = int(1e1)
-
 config = (  # 1. Configure the algorithm,
     PPOConfig()
-    .environment("BreakoutNoFrameskip-v4",
-        clip_rewards=True, clip_actions=True)
+    .rl_module(_enable_rl_module_api=True)
+    .environment("BreakoutNoFrameskip-v4", clip_rewards=True,
+        env_config={"frameskip" : 1, "full_action_space" : False,
+        "repeat_action_probability" : 0.0})
     .resources(num_gpus=1, num_gpus_per_learner_worker=1)
     .rollouts(num_rollout_workers=10, num_envs_per_worker=5,
-        observation_filter="NoFilter",
-        batch_mode="truncate_episodes")
+        rollout_fragment_length="auto")
     .framework("torch")
-    .training(model={"vf_share_layers": True, "conv_filters": None,
-            "conv_activation": "relu", "fcnet_activation": "tanh",
-            "fcnet_hiddens": [256, 256], "free_log_std": False,
-            "no_final_linear": False, "use_lstm": False, "framestack": True,
-            "dim": 84, "grayscale": False, "zero_mean": True,
-            "custom_model": None},
-        entropy_coeff=0.01, train_batch_size=5000, num_sgd_iter=10,
-        lr_schedule=[[0, 5e-5]],
-        vf_clip_param=10.0, kl_coeff=0.5, lambda_=0.95,
-        clip_param=0.1, sgd_minibatch_size=500)
+    .training(_enable_learner_api=True, model={"vf_share_layers": True,
+            "conv_filters" : [[16, 4, 2], [32, 4, 2], [64, 4, 2], [128, 4, 2]],
+            "conv_activation": "relu", "post_fcnet_hiddens": [256]},
+        entropy_coeff=0.01, train_batch_size=4000, num_sgd_iter=10,
+        lr_schedule=[[0, 1e-4]], vf_clip_param=10.0, kl_coeff=0.5,
+        lambda_=0.95, clip_param=0.1, sgd_minibatch_size=1000,
+        grad_clip=100.0, grad_clip_by="global_norm")
     .evaluation(evaluation_num_workers=2, evaluation_duration=10,
         evaluation_interval=10)
 )
@@ -31,7 +27,7 @@ algo = config.build()  # 2. build the algorithm,
 
 #algo = Algorithm.from_checkpoint("/home/winstongrenier/rlPPO1/breakoutChecks1/iter")
 
-for i in range(100):
+for i in range(10):
     print("Start iter", i)
     train_dict = algo.train()  # 3. train it,
     print("Time Total s:", train_dict["time_total_s"])
@@ -39,7 +35,7 @@ for i in range(100):
     iter_num = train_dict["training_iteration"]
     print("Iteration:", iter_num)
     if "evaluation" in train_dict:
-        pprint(train_dict["evaluation"]["sampler_results"])
+        pprint(train_dict)
         save_res = algo.save(checkpoint_dir=\
             "/home/winstongrenier/rlPPO1/breakoutChecks1/iter" + str(iter_num))
         print("SAVED AT", save_res.checkpoint.path)
